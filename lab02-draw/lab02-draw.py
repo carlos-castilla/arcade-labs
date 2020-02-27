63e6b196c565f60e5f5ee286491906eaf39ad9b4
carlos-castilla <carlos.castilla@edu.uah.es>
2020-02-27 20:46:51 +0100

    animacion gif

--- lab02-draw/lab02-draw.py
+++ lab02-draw/lab02-draw.py
@@ -5,6 +5,26 @@ arcade.open_window(800, 600, "Drawing Example")
 # Cielo
 arcade.set_background_color(arcade.color.DARK_BLUE)
 
+def iniciar():
+    arcade.draw_triangle_filled(350 , 190, 800, 120, 800, 300, [255, 255, 0])
+
+    arcade.draw_lrtb_rectangle_filled(50, 400, 225, 130, arcade.color.RED)
+
+    arcade.draw_arc_filled(410, 190, 50, 55, arcade.color.ASH_GREY, 90, 270)
+
+    arcade.draw_lrtb_rectangle_filled(50 , 300 , 300, 150, arcade.color.RED)
+
+    arcade.draw_arc_filled(125 , 127, 100, 110, arcade.color.DARK_RED, 0, 180)
+    arcade.draw_arc_filled(320 , 127, 100, 110, arcade.color.DARK_RED, 0, 180)
+
+    arcade.draw_lrtb_rectangle_filled(190 , 300 , 290, 226, arcade.color.BLUE_BELL)
+    arcade.draw_lrtb_rectangle_filled(70, 180 , 290, 226, arcade.color.BLUE_BELL)
+
+    arcade.draw_circle_filled(125 , 137, 37, arcade.color.BLACK)
+    arcade.draw_circle_filled(125 , 137, 20, arcade.color.ASH_GREY)
+    arcade.draw_circle_filled(320 , 137, 37, arcade.color.BLACK)
+    arcade.draw_circle_filled(320 , 137, 20, arcade.color.ASH_GREY)
+
 ################################################################################
 def coche(x):
     #--Coche--#
@@ -42,15 +62,26 @@ def on_draw(delta_time):
     arcade.draw_lrtb_rectangle_filled(0,800,100,0, arcade.color.SLATE_GRAY)
 
 
-    coche(on_draw.coche_x)
-    on_draw.coche_x +=1
-
     luna(on_draw.luna_x)
     on_draw.luna_x +=1
+
+    coche(on_draw.coche_x)
+    on_draw.coche_x += 1
+
+    if on_draw.coche_x>800:
+        iniciar()
+        on_draw.coche_x=1
+
+
+
+
+
+
 
 
 on_draw.coche_x=1
 on_draw.luna_x=1
+
 
 ################################################################################
 
