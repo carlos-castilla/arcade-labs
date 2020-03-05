7988c12c506f9908437515de42f8f42c2056a506
carlos-castilla <carlos.castilla@edu.uah.es>
2020-03-05 20:26:10 +0100

    juego texto

---
+++ lab04-camel/lab04-camel.py
@@ -0,0 +1,101 @@
+import random
+
+def main():
+
+    miles=0
+    thirst=0
+    tire=0
+    natives=-20
+    canteen=3
+
+    print("Welcome to Camel!")
+    print("You have stolen a camel to make your way across the great Mobi desert.")
+    print("The natives want their camel back and are chasing you down! Survive your")
+    print("desert trek and out run the natives.")
+
+    done = False
+    while not done:
+        print ("A. Drink from your canteen.")
+        print ("B. Ahead moderate speed.")
+        print ("C. Ahead full speed.")
+        print ("D. Stop for the night.")
+        print ("E. Status check.")
+        print ("Q. Quit")
+
+        respuesta=input("What is your choice? ")
+
+
+# ------------------------------THIRST---------------------------------------------#
+        if thirst > 4 and thirst < 7: print ("You are thirsty")
+
+        if thirst > 6:
+            print ("You died of thirst")
+            done = True
+
+
+# -------------------------------TIRED---------------------------------------------#
+        if tire > 5 and tire < 9 and not done: print("Your camel is getting tired")
+
+        if thirst > 8 and not done:
+            print("Your camel is dead")
+            done = True
+# -------------------------------NATIVES-------------------------------------------#
+        if miles-natives < 15 and not done: print ("The natives are getting close!")
+
+        if natives == miles and not done:
+            print ("The navites caught you")
+            done = True
+
+
+# ---------------------------------WIN---------------------------------------------#
+        if miles >= 200 and not done:
+            print ("VICTORY!")
+            done = True
+
+
+# -------------------------------OASIS---------------------------------------------#
+        if random.randint(1,100)<=20:
+            print ("Oasis!")
+            print ("Canteen refilled, thirsty reseted and camel rested")
+            canteen=3
+            thirst=0
+            tire=0
+
+
+# -------------------------------BUCLE---------------------------------------------#
+        if respuesta.upper()=="Q":
+            done = True
+            print ("THE END")
+
+        elif respuesta.upper() == "E":
+            print ("Miles: " , miles)
+            print ("Canteen: " , canteen)
+            print ("The natives are ", miles-natives , " miles behind you.")
+
+        elif respuesta.upper() == "D":
+            tire=0
+            print ("The camel is happy")
+            natives+=random.randint(7,14)
+
+        elif respuesta.upper() == "C":
+            miles+=random.randint(10,20)
+            print("Total miles: ", miles)
+            thirst+=1
+            tire+=random.randint(1,3)
+            natives+=random.randint(7,14)
+
+        elif respuesta.upper() == "B":
+            miles+=random.randint(5,12)
+            print ("Total miles: ", miles)
+            thirst+=1
+            tire+=1
+            natives+=random.randint(7,14)
+
+        elif respuesta.upper() == "A":
+            if canteen!=0:
+                canteen-=1
+                thirst=0
+                print ("Thirst set to 0")
+            else: print ("No drinks left")
+
+main()
